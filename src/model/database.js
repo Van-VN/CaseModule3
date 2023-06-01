add3d5aa065d280b4814891584547d4a1574f663
Van Vu <vanvn.vu@gmail.com>
2023-06-01 11:25:06 +0700

    Added DB

---
+++ src/model/database.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+class Database {
+  constructor() {
+    this.host = "localhost";
+    this.user = "root";
+    this.password = "123";
+    this.database = "jav_db";
+  }
+
+  connect() {
+    console.log("Connected");
+    return mysql.createConnection({
+      host: this.host,
+      user: this.user,
+      password: this.password,
+      database: this.database,
+    });
+  }
+}
+
+module.exports = new Database();
